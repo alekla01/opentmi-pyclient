47cc2b8060e2c77a5631273a6e4907c5ac481aa9
Jussi Vatjus-Anttila <jussiva@gmail.com>
2017-11-18 18:34:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='opentmi_client',
-      version='0.2.1',
+      version='0.2.2',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
