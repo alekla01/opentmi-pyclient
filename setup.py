9d04b25d5fd88ad67a050d2e315c147db06513f8
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-05-07 22:34:42 +0300

    bumb version (#14)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='opentmi_client',
-      version='0.2.2',
+      version='0.3.0',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
