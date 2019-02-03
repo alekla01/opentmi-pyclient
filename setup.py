44cb7c256737819f8de56003be12aa8fce9180e6
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2019-02-03 15:17:08 +0200

    versio update (#34)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools.command.install import install
 DESCRIPTION = "opentmi-client"
 OWNER_NAMES = 'Jussi Vatjus-Anttila'
 OWNER_EMAILS = 'jussiva@gmail.com'
-VERSION = '0.4.1'
+VERSION = '0.5.0'
 
 # Utility function to cat in a file (used for the README)
 def read(fname):
